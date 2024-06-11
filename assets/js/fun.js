2f4cc9b1a0de4c9021a309755e9337506895d6dc
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-06-11 18:09:40 +0200

    da compleatre

--- assets/js/fun.js
+++ assets/js/fun.js
@@ -1,13 +1,26 @@
-// let formName = document.getElementsById("formName");
+let formName = document.getElementById("formName");
 const campoNome = document.getElementById("name");
+const cardRow = document.getElementById("cardRow");
+const save = document.getElementById("save");
+const reset = document.getElementById("reset");
 
 console.log(formName);
 
 class Name {
-  constructor(name) {
-    this.name = name;
+  constructor(a) {
+    this.name = a;
   }
 }
+
+const card = (obg) => {
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.innerText = names;
+  div.appendChild(p);
+  div.classList.add("col");
+
+  document.body.appendChild(div);
+};
 
 let names = [];
 
@@ -19,4 +32,13 @@ formName.onsubmit = (event) => {
   console.log(names);
   localStorage.setItem("events-memory", JSON.stringify(names));
 };
+console.log(names);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const nameSave = localStorage.getItem("events-memory");
+  const nameStarSave2 = JSON.parse(nameSave);
+  console.log(nameStarSave2);
+  nameStarSave2.push;
+});
+
 console.log(names);
